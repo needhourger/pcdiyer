31437db31228fadba29b90c22a3d08f7130539e5
needhourger <chainyuuki@gmail.com>
2024-04-22 11:22:49 +0800

    feat: index

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,20 @@
 </script>
 
 <template>
-  <div>
+  <div class="h-screen bg-gradient-to-b from-black via-purple-950 to-pink-400 backdrop-filter backdrop-blur-lg">
+    <div class="w-full text-center p-8">
+      <span class="text-9xl font-bold text-center jersey-10-regular">PC-DIYer</span>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Jersey+10&display=swap');
+
+.jersey-10-regular {
+  font-family: "Jersey 10", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
 </style>
