332c7088dfaab67a14db1a15e5eb48cc2675c9e8
needhourger <chainyuuki@gmail.com>
2024-04-28 16:32:36 +0800

    refactor: base form

---
+++ src/components/BaseForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <el-form label-position="top" inline :model="form">
+      <el-form-item v-for="item, i in formLabels" :key="i" :label="item.label" :prop="item.prop" size="large"
+        :class="item.class">
+        <el-select v-if="item.type === 'select'" filterable allow-create v-model="form[`${item.prop}`]" :placeholder="item.placeholder">
+          <el-option v-for="option, index in item.options" :key="index" :label="option" :value="option"></el-option>
+        </el-select>
+        <el-input v-if="item.type === 'input'" :placeholder="item.placeholder" v-model="form[`${item.prop}`]"></el-input>
+        <el-input v-if="item.type ==='number'" v-model.number="form[`${item.prop}`]" :prefix-icon="Money"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script setup>
+import { Money } from "@element-plus/icons-vue"
+defineProps({
+  formLabels: { type: Array, default: () => ([])}
+})
+const form = defineModel()
+</script>
+<style lang="less" scoped>
+.el-form-item {
+  margin: 0;
+  margin-bottom: 0.5rem;
+  padding: 0 0.5rem;
+  --el-form-label-font-size: 1.4rem;
+  --el-text-color-regular: #fff;
+}
+.el-select {
+  :deep(.el-select__wrapper) {
+    font-size: 1.2rem;
+  }
+}
+.el-input {
+  font-size: 1.2rem;
+}
+</style>
