1d197d8cca17be5ecce26c777f8b2b3a385acfd5
needhourger <chainyuuki@gmail.com>
2024-04-29 18:05:50 +0800

    feat: update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,8 @@
 <template>
-  <div class="w-full text-center p-10 pb-0 flex items-start">
+  <div class="w-full text-center p-10 pb-0 flex items-start" id="stars">
     <span class="text-9xl font-bold">PC-DIYer</span>
     <div class="text-2xl font-bold ml-auto mt-auto">Total Price: 1000$</div>
   </div>
 </template>
-<script setup>
-</script>
-<style lang="scss" scoped>
-
+<style lang="less" scoped>
 </style>
