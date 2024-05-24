4ac2b95bb5657804433b34c10fa3c5e392bb8551
needhourger <chainyuuki@gmail.com>
2024-05-24 11:17:55 +0800

    feat: add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/Home.vue"
+import NotFoundView from "../views/404.vue"
 
 const routes = [
   {
     path: '/',
     component: HomeView
   },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFoundView
+  }
 ]
 const router = createRouter({
   history: createWebHistory(),
---
+++ src/views/404.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full flex justify-center p-10 flex-col items-center">
+    <div class="text-3xl p-10">Oops! Something went wrong ... Take a break</div>
+    <div class="text-9xl underline underline-offset-auto">404 Page Not Found</div>
+    <img class="p-10" src="https://readme-jokes.vercel.app/api?bgColor=transparent&hideBorder="/>
+  </div>
+</template>
