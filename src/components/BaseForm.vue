3e9918c535401dda723caf2f808977a2cb25bccb
needhourger <chainyuuki@gmail.com>
2024-08-05 15:33:14 +0800

    feat: advance configuration

--- src/components/BaseForm.vue
+++ src/components/BaseForm.vue
@@ -10,71 +10,34 @@
       </el-button>
     </div>
     <el-form label-position="top" inline :model="form" v-if="form">
-      <el-form-item v-for="item, i in formLabels" :key="i" :label="item.label" :prop="item.prop" size="large"
-        :class="item.class">
-        <el-select v-if="item.type === 'select'" filterable allow-create v-model="form[`${item.prop}`]"
-          :placeholder="item.placeholder">
-          <el-option v-for="option, index in item.options" :key="index" :label="option" :value="option"></el-option>
-        </el-select>
-        <el-input v-if="item.type === 'input'" :placeholder="item.placeholder" v-model="form[`${item.prop}`]"
-          :prefix-icon="item.prefixIcon" :suffix-icon="item.suffixIcon">
-          <template #prefix>
-            <span>{{ item.prefix }}</span>
-          </template>
-          <template #suffix>
-            <span>{{ item.suffix }}</span>
-          </template>
-        </el-input>
-        <el-input v-if="item.type === 'number'" v-model.number="form[`${item.prop}`]" :prefix-icon="item.prefixIcon"
-          :suffix-icon="item.suffixIcon">
-          <template #prefix>
-            <span>{{ item.prefix }}</span>
-          </template>
-          <template #suffix>
-            <span>{{ item.suffix }}</span>
-          </template>
-        </el-input>
-        <el-input-number v-if="item.type==='count'" v-model.number="form[`${item.prop}`]" style="margin-left: auto;"></el-input-number>
-      </el-form-item>
+      <FormItemsTemplate v-model="form" :formLabels="commonFormLabels"/>
+      <el-collapse class="w-full mx-3">
+        <el-collapse-item title="Advance configurations">
+          <FormItemsTemplate v-model="form" :formLabels="advanceFormLabels"/>
+        </el-collapse-item>
+      </el-collapse>
     </el-form>
   </div>
 </template>
 <script setup>
 import { RemoveFilled } from "@element-plus/icons-vue"
-defineProps({
+import FormItemsTemplate from "./FormItemsTemplate.vue";
+import { computed } from "vue";
+const props = defineProps({
   formLabels: { type: Array, default: () => ([]) }
 })
 const emits = defineEmits(["remove"])
 const form = defineModel()
+const COMMON_FORM_LABEL = ["model","price","count","link"]
+const advanceFormLabels = computed(() => {
+  return props.formLabels.filter(item => !COMMON_FORM_LABEL.includes(item.prop))
+})
+const commonFormLabels = computed(() => {
+  return props.formLabels.filter(item => COMMON_FORM_LABEL.includes(item.prop))
+})
 </script>
 <style lang="less" scoped>
-.el-form-item {
-  margin: 0;
-  margin-bottom: 0.5rem;
-  padding: 0 0.5rem;
-  --el-form-label-font-size: 1.6rem;
-  --el-text-color-regular: #fff;
-}
-
-.el-select {
-  :deep(.el-select__wrapper) {
-    font-size: 1.2rem;
-    line-height: 1.4rem;
-  }
-  :deep(.el-select__placeholder) {
-    color: #fffc;
-  }
-}
-
-.el-input {
-  --el-input-placeholder-color: #fffc;
-  font-size: 1.2rem;
-}
-
-.el-input-number {
-  --el-fill-color-light: #0004;
-  :deep(.el-input__inner) {
-    font-size: 1.2rem;
-  }
+.el-collapse {
+  --el-collapse-header-font-size: 1.4rem;
 }
 </style>
---
+++ src/components/FormItemsTemplate.vue
@@ -0,0 +1,68 @@
+<template>
+  <el-form-item v-for="item, i in formLabels" :key="i" :label="item.label" :prop="item.prop" size="large"
+    :class="item.class">
+    <el-select v-if="item.type === 'select'" filterable allow-create v-model="form[`${item.prop}`]"
+      :placeholder="item.placeholder">
+      <el-option v-for="option, index in item.options" :key="index" :label="option" :value="option"></el-option>
+    </el-select>
+    <el-input v-if="item.type === 'input'" :placeholder="item.placeholder" v-model="form[`${item.prop}`]"
+      :prefix-icon="item.prefixIcon" :suffix-icon="item.suffixIcon">
+      <template #prefix>
+        <span>{{ item.prefix }}</span>
+      </template>
+      <template #suffix>
+        <span>{{ item.suffix }}</span>
+      </template>
+    </el-input>
+    <el-input v-if="item.type === 'number'" v-model.number="form[`${item.prop}`]" :prefix-icon="item.prefixIcon"
+      :suffix-icon="item.suffixIcon">
+      <template #prefix>
+        <span>{{ item.prefix }}</span>
+      </template>
+      <template #suffix>
+        <span>{{ item.suffix }}</span>
+      </template>
+    </el-input>
+    <el-input-number v-if="item.type === 'count'" v-model.number="form[`${item.prop}`]"
+      style="margin-left: auto;"></el-input-number>
+  </el-form-item>
+</template>
+<script setup>
+defineProps({
+  formLabels: { type: Array, default: () => ([]) }
+})
+const form = defineModel()
+</script>
+<style lang="less" scoped>
+.el-form-item {
+  margin: 0;
+  margin-bottom: 0.5rem;
+  padding: 0 0.5rem;
+  --el-form-label-font-size: 1.6rem;
+  --el-text-color-regular: #fff;
+}
+
+.el-select {
+  :deep(.el-select__wrapper) {
+    font-size: 1.2rem;
+    line-height: 1.4rem;
+  }
+
+  :deep(.el-select__placeholder) {
+    color: #fffc;
+  }
+}
+
+.el-input {
+  --el-input-placeholder-color: #fffc;
+  font-size: 1.2rem;
+}
+
+.el-input-number {
+  --el-fill-color-light: #0004;
+
+  :deep(.el-input__inner) {
+    font-size: 1.2rem;
+  }
+}
+</style>
