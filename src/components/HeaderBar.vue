edacd351ed5167473f479cea39f4bb5f7284174a
needhourger <chainyuuki@gmail.com>
2024-08-07 17:58:39 +0800

    feat: add github link

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,34 +1,46 @@
 <template>
-  <div class="w-full h-10 py-3 px-10 flex items-center justify-end">
-    <el-tooltip effect="dark" :content="$t('save2Image')">
-      <el-button size="large" class="w-8" text @click="save2Image()">
-        <el-icon color="#FFF"><PictureRounded/></el-icon>
-      </el-button>
-    </el-tooltip>
-    <el-tooltip effect="dark" :content="$t('share')">
-      <el-button @click="handleShare" size="large" class="w-8" text>
-        <el-icon color="#fff"><Share/></el-icon>
-      </el-button>
-    </el-tooltip>
-    <el-popover :width="200"
-      placement="bottom-start" :title="$t('language')" trigger="hover">
-      <template #reference>
-        <el-button size="large" text class=" w-9">
-          <el-icon>
-            <img src="@/assets/images/global.svg">
+  <div class="w-full h-10 py-3 px-10 flex items-center justify-between">
+    <div>
+      <el-tooltip effect="dark" content="github">
+        <el-button size="large" class="w-8" text @click="redirect2Github">
+          <el-icon size="large">
+            <img src="@/assets/images/github.svg">
           </el-icon>
         </el-button>
-      </template>
-      <div class="flex flex-col items-center justify-center">
-        <el-button v-for="lang,index in languages" :key="index"
-          @click="handleChangeLang(lang)"
-          class="w-full mx-2 my-1">{{ lang }}</el-button>
-      </div>
-    </el-popover>
+      </el-tooltip>
+    </div>
+    <div>
+      <el-tooltip effect="dark" :content="$t('save2Image')">
+        <el-button size="large" class="w-8" text @click="save2Image">
+          <el-icon color="#FFF">
+            <PictureRounded />
+          </el-icon>
+        </el-button>
+      </el-tooltip>
+      <el-tooltip effect="dark" :content="$t('share')">
+        <el-button @click="handleShare" size="large" class="w-8" text>
+          <el-icon color="#fff">
+            <Share />
+          </el-icon>
+        </el-button>
+      </el-tooltip>
+      <el-popover :width="200" placement="bottom-start" :title="$t('language')" trigger="hover">
+        <template #reference>
+          <el-button size="large" text class=" w-9">
+            <el-icon>
+              <img src="@/assets/images/global.svg">
+            </el-icon>
+          </el-button>
+        </template>
+        <div class="flex flex-col items-center justify-center">
+          <el-button v-for="lang, index in languages" :key="index" @click="handleChangeLang(lang)"
+            class="w-full mx-2 my-1">{{ lang }}</el-button>
+        </div>
+      </el-popover>
+    </div>
   </div>
-  <ImageTemplate :show="imageTemplateShow"/>
+  <ImageTemplate :show="imageTemplateShow" />
 </template>
-
 <script setup>
 import { computed, nextTick, ref } from 'vue';
 import i18nGlobal from '../i18n';
@@ -47,7 +59,7 @@ const languages = computed(() => {
 })
 const handleChangeLang = (lang) => {
   locale.value = lang
-  localStorage.setItem('language',lang)
+  localStorage.setItem('language', lang)
   window.location.reload()
 }
 const handleShare = () => {
@@ -67,7 +79,7 @@ const save2Image = () => {
       const imageURL = canvas.toDataURL('image/png')
       const downloadTime = new Date()
       const imageName = `pcdiyer-${downloadTime.toUTCString()}.png`
-      createDownload(imageURL,imageName)
+      createDownload(imageURL, imageName)
     }).finally(() => {
       setTimeout(() => {
         imageTemplateShow.value = false
@@ -75,8 +87,9 @@ const save2Image = () => {
     })
   })
 }
+
+const redirect2Github = () => {
+  window.open('https://github.com/needhourger/pcdiyer','_blank')
+}
 </script>
-
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
